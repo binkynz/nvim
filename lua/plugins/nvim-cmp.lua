8cbd6143b6bca56b68c50fcbb488060377d426fd
Sean Regsiter <seancregister@gmail.com>
2023-07-01 14:41:25 +1200

    use aerial as a dynamic doc search

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -50,11 +50,6 @@ return {
 			},
 			formatting = {
 				fields = { "kind", "abbr" },
-				format = function(_, vim_item)
-					vim_item.kind = "[" .. (icons[vim_item.kind] or "") .. "]"
-
-					return vim_item
-				end,
 			},
 		})
 	end,
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,15 +6,16 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		{ "nvim-telescope/telescope-file-browser.nvim" },
+		"nvim-telescope/telescope-file-browser.nvim",
+		{ "stevearc/aerial.nvim", opts = {} },
 	},
 
 	keys = {
 		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files in current working directory" },
 		{ "<leader>fs", "<cmd>Telescope live_grep<cr>", desc = "Find string in current working directory" },
 		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "List help tags" },
-		{ "<leader>ds", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", desc = "Finds all dynamic symbols" },
 		{ "<leader>fb", "<cmd>Telescope file_browser<cr>", desc = "File Browser" }, -- https://github.com/nvim-telescope/telescope-file-browser.nvim#mappings
+		{ "<leader>fd", "<cmd>Telescope aerial<cr>", desc = "Aerial document symbols" },
 	},
 
 	opts = {},
@@ -24,5 +25,6 @@ return {
 		telescope.setup(opts)
 		telescope.load_extension("fzf")
 		telescope.load_extension("file_browser")
+		telescope.load_extension("aerial")
 	end,
 }
